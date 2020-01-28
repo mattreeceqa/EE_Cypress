d8742fbbca543fd30c31439df6244d8d3c076938
matt.reece <matthew.reece@coopdigital.co.uk>
2020-01-28 12:37:44 +0000

    commit of full project

---
+++ cypress/integration/BookingTests.spec.js
@@ -0,0 +1,63 @@
+///<reference types="cypress" />
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false;
+});
+
+const globals = require('../fixtures/globals.json');
+const bookingpage_selector = require('../fixtures/bookingpage_selectors.json');
+
+describe('Hotel Booking Tests', function () {
+  beforeEach(function () {
+    cy.visit(globals.homePage);
+  });
+
+  it('Loads the homepage elements', function () {
+    cy.title().should('contain', 'Hotel booking form')
+    cy.getPageElements();
+    cy.assertText(bookingpage_selector.firstName, 'Firstname');
+    cy.assertText(bookingpage_selector.Surname, 'Surname');
+    cy.assertText(bookingpage_selector.Price, 'Price');
+    cy.assertText(bookingpage_selector.Deposit, 'Deposit');
+    cy.assertText(bookingpage_selector.Checkin, 'Check-in');
+    cy.assertText(bookingpage_selector.Checkout, 'Check-out');
+  })
+
+  it('Add a booking with deposit', function () {
+    cy.wait(1000)
+    cy.getBookingCount().then(existingbookings => {
+      cy.addDepositBookingData();
+      cy.saveBooking();
+      cy.wait(5000);
+      cy.verifyFirstName('First');
+      cy.verifySurname('Last');
+      cy.verifyDeposit('true')
+      cy.getBookingCount().should('be.greaterThan', existingbookings)
+    })
+  })
+
+  it('Add a booking with no deposit', function () {
+    cy.wait(1000)
+    cy.getBookingCount().then(existingbookings => {
+      cy.addNoDepositBookingData();
+      cy.saveBooking();
+      cy.wait(5000);
+      cy.verifyFirstName('Firstnd');
+      cy.verifySurname('Lastnd');
+      cy.verifyDeposit('false');
+      cy.getBookingCount().should('be.greaterThan', existingbookings)
+    })
+  });
+
+  it('Add and then delete a booking', function () {
+    cy.addDepositBookingData();
+    cy.saveBooking();
+    cy.wait(5000);
+    cy.getBookingCount().then(existingbookings => {
+      cy.wait(1000);
+      cy.deleteBooking();
+      cy.getBookingCount().should('be.lessThan', existingbookings)
+    })
+  })
+
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,69 @@
+const bookingpage = require('../fixtures/bookingpage_selectors.json');
+
+Cypress.Commands.add('assertText', (element, text) => {
+  cy.get(element).should('have.text', text);
+});
+
+Cypress.Commands.add('getPageElements', () => {
+  cy.get(bookingpage.h1);
+  cy.get(bookingpage.firstName);
+  cy.get(bookingpage.Surname);
+  cy.get(bookingpage.Price);
+  cy.get(bookingpage.Checkin);
+  cy.get(bookingpage.Checkout);
+});
+
+Cypress.Commands.add('getBookingCount', () => {
+  cy.get('.row[id]').its('length')
+});
+
+Cypress.Commands.add('getLastBooking', () => {
+  var bookingid = cy.get('.row[id]').last
+  console.log('booking id is', bookingid)
+});
+
+Cypress.Commands.add('addDepositBookingData', () => {
+  cy.get(bookingpage.inputFirst).type('First')
+  cy.get(bookingpage.inputSurname).type('Last')
+  cy.get(bookingpage.inputPrice).type('99')
+  cy.get(bookingpage.selectDeposit).trigger('mouseover').select('true')
+  cy.get(bookingpage.inputCheckIn).type('2020-01-20')
+  cy.get(bookingpage.inputCheckOut).type('2020-01-21')
+  cy.get(bookingpage.firstName).click()//click off to close date picker
+});
+
+Cypress.Commands.add('addNoDepositBookingData', () => {
+  cy.get(bookingpage.inputFirst).type('Firstnd')
+  cy.get(bookingpage.inputSurname).type('Lastnd')
+  cy.get(bookingpage.inputPrice).type('99')
+  cy.get(bookingpage.selectDeposit).trigger('mouseover').select('false')
+  cy.get(bookingpage.inputCheckIn).type('2020-01-20')
+  cy.get(bookingpage.inputCheckOut).type('2020-01-21')
+  cy.get(bookingpage.firstName).click()//click off to close date picker
+});
+
+Cypress.Commands.add('saveBooking', () => {
+  cy.get(bookingpage.Save).click()
+});
+
+Cypress.Commands.add('deleteBooking', () => {
+  cy.get(bookingpage.Delete).last().click()
+});
+
+Cypress.Commands.add('verifyFirstName', (value) => {
+  cy.get('.row[id]').last().children('.col-md-2').should(($div) => {
+    expect($div.get(0).innerText).to.eq(value)
+  })
+});
+
+Cypress.Commands.add('verifySurname', (value) => {
+  cy.get('.row[id]').last().children('.col-md-2').should(($div) => {
+    expect($div.get(1).innerText).to.eq(value)
+  })
+});
+
+Cypress.Commands.add('verifyDeposit', (value) => {
+  cy.get('.row[id]').last().children('.col-md-2').should(($div) => {
+    expect($div.get(2).innerText).to.eq(value)
+  })
+});
